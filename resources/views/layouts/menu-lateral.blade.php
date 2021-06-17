9f69e3a0043d62fb52a194af9ffd053f0f9f5f53
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-06-17 11:09:45 -0300

    LHenrique_Icones_3ªAlteracao

--- resources/views/layouts/menu-lateral.blade.php
+++ resources/views/layouts/menu-lateral.blade.php
@@ -3,20 +3,32 @@
 {{-- <aside class="main-sidebar sidebar-dark-primary elevation-4" style="overflow-y: scroll"> --}}
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="">
     <!-- Brand Logo -->
-    <a href="{{ route('home') }}" class="brand-link">
-        {{-- <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-         class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
-        <span class="brand-text font-weight-light">Bentley Brasil</span>
+    <a href="https://sistema.bentleybrasil.com.br/home" class="brand-link" style="
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    justify-content: center;">
+    <span class="brand-text font-weight-light">
+        <img height="70" src="https://sistema.bentleybrasil.com.br/img/logo-empresa-br.png">
+    </span>
     </a>
-
+    <br>
     <!-- Sidebar -->
-    <div class="info">
+    <div class="info" style="
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    justify-content: center;">
+        <div class="col">
         <a class="d-block">{{ Auth::user()->name }}</a>
+        </div>
 
+        <div class="col">
         <a class="btn btn-danger btn-sm" href="{{ route('logout') }}" onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
             Sair
         </a>
+    </div>
 
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
