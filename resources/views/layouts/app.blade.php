5269963cc24f65697169d717f8b5678112b1dc91
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-06-17 09:48:41 -0300

    LHenriqueIcones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,35 @@
 
 <head>
 
-    {{-- <iframe src="https://beta.simet.nic.br/widget.html" height="320" width="780" style="border:none" ></iframe> --}}
-
-    {{-- <iframe src="https://beta.simet.nic.br/widget.html" height="780" width="430" style="border:none" ></iframe> --}}
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta http-equiv="expires" content="-1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+	<meta name="theme-color" content="#081638">
+
+	<link rel="icon" sizes="192x192" href="img/icone-192.png">
+	<title>Bentley Brasil</title>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="icon" sizes="16x16 32x32 64x64" href="favicon.ico">
+	<link rel="icon" type="image/png" sizes="192x192" href="img/icone-192.png">
+	<link rel="icon" type="image/png" sizes="160x160" href="img/icone-160.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/icone-96.png">
+	<link rel="icon" type="image/png" sizes="64x64" href="img/icone-64.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/icone-32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/icone-16.png">
+	<link rel="apple-touch-icon" href="img/icone-57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/icone-114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/icone-72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/icone-144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/icone-60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/icone-120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/icone-76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/icone-152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/icone-180.png">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
